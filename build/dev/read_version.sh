02f331c8488268c093921be568cc4bca59c7cb72
Fernanda Serra <fernandafmst@decea.mil.br>
2024-07-25 10:22:14 -0300

    Changed reference to dummy auth to use kong gateway URL

--- build/dev/read_version.sh
+++ build/dev/read_version.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 # Retrieve token from dummy OAuth server
 ACCESS_TOKEN=$(curl --silent \
-    "http://localhost:8085/token?grant_type=client_credentials&scope=interuss.versioning.read_system_versions&intended_audience=localhost&issuer=localhost&sub=check_scd" \
+    "http://172.17.0.1:8443/token?grant_type=client_credentials&scope=interuss.versioning.read_system_versions&intended_audience=localhost&issuer=localhost&sub=check_scd" \
 | python extract_json_field.py 'access_token')
 
 curl --silent -X GET  \
